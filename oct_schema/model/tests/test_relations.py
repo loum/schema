38a3438396106493ba805213389ea774accc3cce
Lou Markovski <lou.markovski@gmail.com>
2014-07-07 17:21:55 +1000

    task/us99-relations-observations
    Relations lookup returns observations that are associated with a facility
    
    oct_schema/model_base.py
    * new method search() that extracts documents from the
      collection context associated with a given token
    
    oct_schema/model/relations.py
    * New method observation_serach()" that acts as a wrapper
      around a Relations Collection context that filters out  .was
      observed. elements from the ._id. key with a given token
      regular expression

--- oct_schema/model/tests/test_relations.py
+++ oct_schema/model/tests/test_relations.py
@@ -9,6 +9,19 @@ from oct.utils.log import set_log_level
 
 class TestRelations(unittest2.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        fixtures_dir = os.path.join('oct_schema',
+                                    'model',
+                                    'tests',
+                                    'fixtures')
+
+        f = os.path.join(fixtures_dir, 'relations.json')
+        oct_schema.model.Relations()._batch_loader(f)
+
+        f = os.path.join(fixtures_dir, 'objects.json')
+        oct_schema.model.Objects()._batch_loader(f)
+
     def test_init(self):
         """Initialise a :class:`oct.model.Relations` object.
         """
@@ -17,10 +30,15 @@ class TestRelations(unittest2.TestCase):
         self.assertIsInstance(o, oct_schema.model.Relations, msg)
         o.rollback
 
-    def test_mim(self):
-        """Test the MongoDB-in-Memory Ming component.
+    def test_insert_and_delete(self):
+        """Test Relations collection insert and delete.
         """
-        id = 'observation|2006-06-10T07:03:00Z|SAMPLEIMAGEID|Kirov CGN|Petr Valikiy (test)'
+        event = 'observation'
+        date = '2006-06-10T07:03:00Z'
+        image = 'SAMPLEIMAGEID'
+        type = 'Kirov CGN'
+        name = 'Petr Valikiy (test)'
+        id = '%s|%s|%s|%s|%s' % (event, date, image, type, name)
         kwargs = {'_id': 'was observed|Kirov CGN|Petr Valikiy (test)',
                   'id': [id]}
         o = oct_schema.model.Relations(**kwargs)
@@ -33,17 +51,18 @@ class TestRelations(unittest2.TestCase):
         msg = 'Relations collection search returned incorrect count'
         self.assertEqual(received, expected, msg)
 
+        # Clean up.
+        kwargs = {'_id': 'was observed|Kirov CGN|Petr Valikiy (test)'}
+        results = oct_schema.model.Relations.query.find(kwargs)
+        set_log_level('INFO')
+        for i in results:
+            i.delete()
+            i.flush
+        set_log_level('DEBUG')
+
     def test_batch_loader(self):
         """Test the batch loader: Kirov CGN wildcard search against _id key.
         """
-        fixtures_dir = os.path.join('oct_schema',
-                                    'model',
-                                    'tests',
-                                    'fixtures')
-
-        f = os.path.join(fixtures_dir, 'relations.json')
-        oct_schema.model.Relations()._batch_loader(f)
-
         regx = re.compile('.*kirov.*', re.IGNORECASE)
         kwargs = {'_id': regx}
         results = oct_schema.model.Relations.query.find(kwargs)
@@ -55,3 +74,24 @@ class TestRelations(unittest2.TestCase):
                     'was observed|Kirov CGN|Unidentified']
         msg = 'Kirov wildcard search against _id error'
         self.assertListEqual(sorted(received), sorted(expected), msg)
+
+    def test_search(self):
+        """Test the Relations context search method.
+        """
+        set_log_level('INFO')
+        received = oct_schema.model.Relations().search(token='Kirov')
+        set_log_level('DEBUG')
+        expected = [u'has component|Kirov CGN|Petr Valikiy (099)',
+                    u'was observed|Kirov CGN|Petr Valikiy (099)',
+                    u'was observed|Kirov CGN|Unidentified']
+        msg = 'Kirov wildcard search against _id error'
+        self.assertListEqual(sorted(received), sorted(expected), msg)
+
+    def test_observation_search(self):
+        """Test the Relations observation search method.
+        """
+        token = 'Kirov CGN'
+
+        set_log_level('INFO')
+        received =  oct_schema.model.Relations().observation_search(token)
+        set_log_level('DEBUG')
--- oct_schema/model_base.py
+++ oct_schema/model_base.py
@@ -2,6 +2,7 @@ import ming
 import ming.odm
 import ming.odm.declarative
 import json
+import re
 from datetime import datetime
 
 import oct_schema
@@ -24,6 +25,9 @@ class ModelBase(ming.odm.declarative.MappedClass):
         the Document was modified
 
     """
+    def __str__(self):
+        return type(self).__name__
+
     __metaclass__ = ming.odm.declarative._MappedClassMeta
 
     class __mongometa__:
@@ -78,5 +82,40 @@ class ModelBase(ming.odm.declarative.MappedClass):
                 record_count += 1
 
         log.debug('Fixture-based records loaded: %d' % record_count)
+
+
+    def search(self, token, regex=False, case_sensitive=True):
+        """Extract documents from the Collection context associated with
+        the *token*.  If *regex* is ``True`` then the token is applied
+        as a regular expression to the search (the default is that
+        *token* is used as a literal).  Similarly, if
+        *case_sensitive* is ``False`` then the token search is case
+        insensitive (default is case sensitive).
+
+        **Args:**
+            *token*: the search token to build into the regular expression
+            to search against the Collection context
+
+        **Kwargs:**
+            *regex*: Control whether *token* is to be built as a regular
+            expression in the search criteria.  Defaults to ``False``
+
+            *case_sensitive*: Control whether *token* is to be used
+            as a case-sensitive search criteria.  Defaults to ``True``
+
+        """
+        log.info('Searching Collection: "%s" with token "%s"' %
+                 (str(self), str(token)))
+
+        kwargs = {}
+        if regex:
+            r = re.compile('.*%s.*' % token, re.IGNORECASE)
+            kwargs['_id'] = r
+        else:
+            kwargs['_id'] = token
+
+        results = self.query.find(kwargs)
+
+        return results
 
 ming.odm.Mapper.compile_all()
